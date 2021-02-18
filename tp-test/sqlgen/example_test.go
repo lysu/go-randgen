3669c2c08418b32a2456883be4fbd741dbcf11d9
lysu <sulifx@gmail.com>
2021-02-18 11:16:15 +0800

    add compare test

--- tp-test/sqlgen/example_test.go
+++ tp-test/sqlgen/example_test.go
@@ -1,7 +1,13 @@
 package sqlgen
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -11,5 +17,109 @@ func TestA(t *testing.T) {
 	gen := NewGenerator(state)
 	for i := 0; i < 200; i++ {
 		fmt.Printf("%s;\n", gen())
+	}
+}
+
+func TestCompareClusterVSNonCluster(t *testing.T) {
+	deep := 20000
+	var deepStr = os.Getenv("DEEP")
+	if len(deepStr) > 0 {
+		var err error
+		deep, err = strconv.Atoi(deepStr)
+		if err != nil {
+			panic(err)
+		}
+	}
+	for {
+		doTest(deep)
+	}
+}
+
+func doTest(deep int) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Printf("FAIL! %v", r)
+		}
+	}()
+	gen := NewGenerator(NewState())
+
+	c, closeC := testConn("ctest")
+	defer closeC()
+	pIfe(c.ExecContext(context.Background(), "set @@tidb_enable_clustered_index=true"))
+
+	nc, closeNc := testConn("nctest")
+	defer closeNc()
+	pIfe(nc.ExecContext(context.Background(), "set @@tidb_enable_clustered_index=false"))
+
+	for i := 0; i < deep; i++ {
+		sql := gen()
+		fmt.Println(sql)
+		if strings.HasPrefix(strings.ToLower(sql), "select") {
+			cRs, cErr := c.QueryContext(context.Background(), sql)
+			ncRs, ncErr := nc.QueryContext(context.Background(), sql)
+			if (cErr == nil && ncErr != nil) || (cErr != nil && ncErr == nil) || (cErr != nil && ncErr != nil && cErr.Error() != ncErr.Error()) {
+				panic(fmt.Sprintf("sql:%s result not match %s vs %s", sql, cErr, ncErr))
+			}
+			if cRs != nil && ncRs != nil {
+				func() {
+					defer cRs.Close()
+					defer ncRs.Close()
+					for {
+						nextC, nextNc := cRs.Next(), ncRs.Next()
+						if (!nextC && nextNc) || (nextC && !nextNc) {
+							panic(fmt.Sprintf("sql:%s result not match %t vs %t", sql, nextC, nextNc))
+							break
+						}
+						if !nextNc && !nextC {
+							break
+						}
+					}
+				}()
+			}
+		} else {
+			_, cErr := c.ExecContext(context.Background(), sql)
+			_, ncErr := nc.ExecContext(context.Background(), sql)
+			if (cErr == nil && ncErr != nil) || (cErr != nil && ncErr == nil) || (cErr != nil && ncErr != nil && cErr.Error() != ncErr.Error()) {
+				panic(fmt.Sprintf("sql:%s result not match %s vs %s", sql, cErr, ncErr))
+			}
+		}
+	}
+}
+
+func testConn(n string) (*sql.Conn, func()) {
+	initDB, err := sql.Open("mysql", `root@tcp(127.0.0.1:4000)/test`)
+	if err != nil {
+		panic(err)
+	}
+	defer initDB.Close()
+	_, err = initDB.ExecContext(context.Background(), "drop database if exists "+n)
+	if err != nil {
+		panic(err)
+	}
+	_, err = initDB.ExecContext(context.Background(), "create database "+n)
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open("mysql", `root@tcp(127.0.0.1:4000)/`+n)
+	if err != nil {
+		panic(err)
+	}
+	c, err := db.Conn(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	return c, func() {
+		c.Close()
+		db.Close()
+	}
+}
+
+func pIfe(vs ...interface{}) {
+	if len(vs) == 0 {
+		return
+	}
+	if e, isError := vs[len(vs)-1].(error); isError {
+		panic(e)
 	}
 }
